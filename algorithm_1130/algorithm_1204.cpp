10826c67313fef6ef8e6f5d6d2c3572169a57731
choyj0920 <choyj0920@naver.com>
2019-12-14 17:27:21 +0900

    Update algorithm_1204.cpp

--- algorithm_1130/algorithm_1204.cpp
+++ algorithm_1130/algorithm_1204.cpp
@@ -1,13 +1,84 @@
 #include <iostream>
 
 using namespace std;
+//4948 베르트랑 공준
+#include <list>
+int a[246913] = { 0,1, };
+int main() {
+	//에라스토테네스 체
+	int max = 0;
+	int n = -1;
+	list<int> list;
+	while (n != 0) {
+		cin >> n;
+		if (n > max) {
+			max = n;
+		}
+		if (n != 0)
+			list.push_back(n);
+	}
+	for (int i = 2; i <= max * 2; i++)
+		for (int j = 2; j * i <= max * 2; j++)
+			a[i * j] = 1;
+
+	std::list<int>::iterator it;
+	for (it = list.begin(); it != list.end(); it++) {
+		int count = 0;
+		for (int t = *it; t < (*it) * 2; ) {
+			if (a[++t] == 0)
+				count++;
+		}
+		cout << count << "\n";
+	}
+}
+
+
+//1929 소수구하기
+/*
+int a[1000001] = { 0,1, };
+int main() {
+	//에라스토테네스 체
+	int M, N; //1을 제외 모두 소수 설정
+	cin >> M >> N;
+	for (int i = 2; i <= N; i++)
+		for (int j = 2; j * i <= N; j++)
+			a[i * j] = 1;
+	for (int i = M; i <= N; i++) {
+		if(!a[i])
+			cout << i << "\n";
+	}
+}*/
+
 //2581 소수
+/*
+int sosoo(int N) {
+	if (N == 1) return 0;
+	for (int i = 2; i < N; i++) {
+		if (N % i == 0) {
+			return 0;
+		}
+	}
+	return 1;
+}
 int main() {
 	int M, N;
 	cin >> M >> N;
+	int count = 0, min;
 
+	for (int i = M; i <= N; i++) {
+		if (sosoo(i)) {
+			if (count == 0)
+				min = i;
+			count += i;
+		}
+	}
+	if (count == 0)
+		cout << -1;
+	else
+		cout << count << "\n" << min;
 
 }
+*/
 
 //1978 소수 찾기 
 /*
