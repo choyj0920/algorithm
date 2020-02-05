cfa69e00f0297022e1ba3a8b7ac1dc681da07fb1
choyj0920 <choyj0920@naver.com>
2020-02-06 02:26:06 +0900

    // 백준 9251번 LCS

--- README.md
+++ README.md
@@ -12,5 +12,34 @@
 
 ### 학습내용:
 
-### 12월 
+## 2020년
+
+
+
+### 2월 
+
+DP알고리즘- LIS알고리즘(+LCS)알고리즘
+
+```cpp
+#include<vector>
+int main() {
+	vector<int> v;
+	int N; //N수열 갯수
+	cin >> N;
+	int k;
+	for (int i = 0; i < N; i++) {
+		cin >> k; //수열을 하나씩
+		auto it = lower_bound(v.begin(), v.end(), k);
+        //
+			*it = k;
+	}
+	cout << v.size() << '\n';	
+}
+
+
+
+
+
+
+```
 
--- algorithm_202002/algorithm_202002.cpp
+++ algorithm_202002/algorithm_202002.cpp
@@ -4,3 +4,26 @@
 #include<algorithm>
 using namespace std;
 
+// น้มุ 9251น๘ LCS
+int DP[1001][1001] = { 0, };
+
+int main() {
+	cin.tie(0);
+	string str1, str2;
+	int len1, len2;
+	cin >> str1 >> str2;
+	len1 = str1.length();
+	len2 = str2.length();
+	for (int i = 1; i <= len1; i++) {
+		
+		for (int j = 1; j <= len2; j++) {
+			if (str1[i - 1] != str2[j - 1]) {
+				DP[i][j] = max(DP[i - 1][j], DP[i][j - 1]);
+			}
+			else {
+				DP[i][j] = DP[i - 1][j - 1] + 1;
+			}
+		}
+	}
+	cout << DP[len1][len2] << '\n';
+}
