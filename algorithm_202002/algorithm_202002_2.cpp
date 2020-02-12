d1c73c4b7a4f966819e6b7edffba284aa1302314
choyj0920 <choyj0920@naver.com>
2020-02-13 00:13:13 +0900

    // 백준 2618번 경찰차

--- algorithm_202002/algorithm_202002_2.cpp
+++ algorithm_202002/algorithm_202002_2.cpp
@@ -6,7 +6,89 @@
 #include<algorithm>
 using namespace std;
 
-// 백준 10942번 팰린드롬?
+// 백준 2618번 경찰차
+#include <stdio.h>
+#include <algorithm>
+#include <string.h>
+#include <vector>
+using namespace std;
+
+int n, w;
+struct col {
+	int x, y;
+	int operator-(const col& aa) {
+		return abs(x - aa.x) + abs(y - aa.y);
+	}
+};
+col fir[1001], sec[1001];
+int dp[1001][1001];
+
+int func(int c1, int c2) {
+	if (c1 == w || c2 == w)
+		return 0;
+
+	int& ref = dp[c1][c2];
+	if (ref != -1)
+		return ref;
+
+	int np = max(c1, c2) + 1;
+	int n1 = fir[c1] - fir[np];
+	int n2 = func(np, c2) + n1;
+
+	int m1 = sec[c2] - sec[np];
+	int m2 = func(c1, np) + m1;
+
+	return ref = min(n2, m2);
+}
+
+vector<int> v;
+void tracking(int c1, int c2) {
+	if (c1 == w || c2 == w)
+		return;
+
+	int np = max(c1, c2) + 1;
+	int n1 = fir[c1] - fir[np];
+	int n2 = dp[np][c2] + n1;
+
+	int m1 = sec[c2] - sec[np];
+	int m2 = dp[c1][np] + m1;
+
+	if (n2 > m2) {
+		v.push_back(2);
+		tracking(c1, np);
+	}
+	else {
+		v.push_back(1);
+		tracking(np, c2);
+	}
+}
+
+int main() {
+	cin.tie(0); cin.sync_with_stdio(false);
+
+	cin >> n >> w;
+
+	fir[0] = { 1,1 };
+	sec[0] = { n,n };
+	for (int i = 1; i <= w; ++i) {
+		cin >> fir[i].x >> fir[i].y;
+		sec[i] = { fir[i].x, fir[i].y };
+	}
+
+	memset(dp, -1, sizeof(dp));
+	cout << func(0, 0) << '\n';
+
+	tracking(0, 0);
+	for (auto a : v)
+		cout << a << '\n';
+
+
+	return 0;
+}
+
+
+// 백준 10942번 팰린드롬? 
+/*
 int arr[2001];
 bool dp[2001][2001];
 int n, m;
@@ -31,7 +113,7 @@ int main() {
 		cin >> s >> e;
 		cout << dp[s][e] << '\n';
 	}
-}
+}*/
 
 
 // 백준 7579번 앱 어려워 잘모르겠다 
