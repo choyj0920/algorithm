3c50c4f0405dbc150e445b96ad65760029008539
choyj0920 <44551544+choyj0920@users.noreply.github.com>
2020-12-18 17:10:09 +0900

    // baek 2482  색상환

--- algorithm_202012/baek 2482.cpp
+++ algorithm_202012/baek 2482.cpp
@@ -21,17 +21,18 @@ int main()
     // 입력
     cin>>n>>k;
 
-    for (int i=0;i<=n;i++){
+    for (int i=0;i<=n;i++){  // 값 초기화 
         dp[i][1] =i;
         dp[i][0] = 1;
     }
 
-    for (int i=2;i<=n;i++){
-        for (int j=2;j<=k; j++){
-            dp[i][j]=(dp[i-2][j-1]+dp[i-1][j]) % mod;
+    for (int i=2;i<=n;i++){ //2칸 2개색칠 부터 -여기에선 선형적으로  원형이라는걸 무시
+        for (int j=2;j<=k&&j<=i; j++){
+            dp[i][j]=(dp[i-2][j-1]+dp[i-1][j]) % mod;       //i칸중 j칸을 색칠하는 경우는 
+                                                            //0~ i-1칸중 j칸을 색칠하는 경우 +  0~i-2칸중 j-1칸을 색칠하는(i칸색칠) 경우           
         }
     }
-    int ans= (dp[n-1][k]+ dp[n-3][k-1]) % mod; //0과 n은 같이 색칠되면 안됨
+    int ans= (dp[n-1][k]+ dp[n-3][k-1]) % mod; //0과 n은 같이 색칠되면 안됨 -원형
     cout<<ans<<'\n';
 
 
