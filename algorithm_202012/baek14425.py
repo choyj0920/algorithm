275fe7f54f763f25f6b9f9dcc5cdb89f0dbde99c
choyj0920 <44551544+choyj0920@users.noreply.github.com>
2020-12-27 00:26:03 +0900

    # baek14425 문자열집합 -trie 풀이 pypy

--- algorithm_202012/baek14425.py
+++ algorithm_202012/baek14425.py
@@ -1,7 +1,10 @@
-# baek14425 문자열집합
+# baek14425 문자열집합 -trie 풀이 pypy로 제출
 import sys
 input= sys.stdin.readline
 
+n,m =map(int,input().split())
+n_word=["" for _ in range(n)]
+m_word=["" for _ in range(m)]
 class Node:
     def __init__(self,key):
         self.key=key
@@ -14,29 +17,34 @@ class Trie:
     def insert(self, word):
         cur =self.head
 
-        for ch in word:
+        for ch in n_word[word]:
             if ch not in cur.child:
                 cur.child[ch]=Node(ch)
             cur = cur.child[ch]
         cur.child['*']=True
 
-def print_trie(l,cur):
-    if '*' in cur.child:
-        return
-    sorted_child=sorted(cur.child)
-    
-    for c in sorted_child:
-        print("--"*l, end='')
-        print(c)
-        print_trie(l+1,cur.child[c])
-    
+    def find(self,word):
+        
+        cur=self.head
+        for ch in m_word[word]:
+            if ch in cur.child:
+                cur=cur.child[ch]
+            else:
+                return False
+        if '*' in cur.child:
+            return True
+        return False
+        
 trie=Trie()
 
-for _ in range(int(input())):
-    foods = input().split()
-    k= int(foods.pop(0))
+for _ in range(n):
+    n_word[_]=input()
+    trie.insert(_)
 
-    trie.insert(foods)
+cnt=0
+for _ in range(m):
+    m_word[_]=input()
+    if trie.find(_):
+        cnt+=1
 
-cur =trie.head
-print_trie(0,cur)
+print(cnt)
