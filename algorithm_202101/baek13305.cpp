6212915542ee5fa0fbe3d92d0491b7a08c4793ca
choyj0920 <44551544+choyj0920@users.noreply.github.com>
2021-01-03 18:42:23 +0900

    // baek13305 주유소

--- algorithm_202101/baek13305.cpp
+++ algorithm_202101/baek13305.cpp
@@ -4,19 +4,20 @@
 #include <string>
 #include <algorithm>
 using namespace std;
+typedef long long ll ;
 
-int arr[100001],oil[100001];
+ll arr[100001],oil[100001];
 int main(){
     int n;
     cin>>n;
-    for (int i=1;i<n;i++) cin>>arr[i];
+    for (int i=0;i<n-1;i++) cin>>arr[i];
     for (int i=0;i<n;i++) cin>>oil[i];
     
-    int answer=0;
-    int minoil=1000000000;
-    for (int i=0;i<n-1;i++){
+    ll answer=oil[0]*arr[0];
+    ll minoil=oil[0];
+    for (int i=1;i<n;i++){
         minoil=min(minoil,oil[i]);
-        answer += (minoil*arr[i+1]);
+        answer += (minoil*arr[i]);
     }
     cout<<answer<<'\n';
 }
