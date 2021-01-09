8e1502e7fbfdb3b56b5f14f433c34d5eb7d2d035
choyj0920 <44551544+choyj0920@users.noreply.github.com>
2021-01-10 03:14:39 +0900

    # 7869 두 원- 수정

--- algorithm_202101/baek7869.py
+++ algorithm_202101/baek7869.py
@@ -10,22 +10,24 @@ d_2 = ((x1-x2)**2) + ((y1-y2)**2) # 중심 거리 제곱
 d=math.sqrt(d_2)
 a= r1*r1            # 반지름1 제곱
 b= r2*r2            # 반지름2 제곱
-if a<b: 
+if a<b: # a를 큰 값으로
     a,b= b,a
     r1,r2=r2,r1
 
-if d>= r1+r2:
+if d_2 > (r1+r2)**2: # 원이 만나지 않는 경우 
     print(round(0,3))
-elif d>= r1:
-    cos_halfT1 =  (d_2-(a-b))/(2*r1*d)
-    cos_halfT2 =  (d_2+(a-b))/(2*r2*d)
+elif d==0:           # 원의 중심이 일치 하는 경우
+    print(round(math.pi*b,3))
+elif d_2+b>= a:      #  경우 1 부채꼴 두개 - 삼각형두개 
+    cos_halfT1 =  (d_2+a-b)/(2*r1*d)
+    cos_halfT2 =  (d_2-a+b)/(2*r2*d)
     t1=(math.acos(cos_halfT1)) * 2
     t2=(math.acos(cos_halfT2)) * 2
 
     S= (a*(t1-math.sin(t1))+b*(t2-math.sin(t2)))/2
     print(round(S,3))
 
-elif d+r2> r1:
+elif d_2> (r1-r2)**2:   # 경우2 
     cos_halfT1 =  (a+d_2-b)/(2*r1*d)
     cos_halfT2 =  (b+d_2-a)/(2*r2*d)
     half_t1=(math.acos(cos_halfT1)) 
