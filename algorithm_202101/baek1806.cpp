abcf08bb71f4441e812402258d7f3d43e8d77023
choyj0920 <44551544+choyj0920@users.noreply.github.com>
2021-01-23 09:04:53 +0900

    baek1806 오류수정

--- algorithm_202101/baek1806.cpp
+++ algorithm_202101/baek1806.cpp
@@ -10,10 +10,10 @@ int  arr[100001]={0,};
 int n,s;
 int main(){
     cin>>n>>s;
-    for (int i=1; i<=n;i++)
+    for (int i=0; i<n;i++)
         cin>>arr[i];
         
-    int start=0,end=0,sum=0,minLen=100001;
+    int start=0,end=0,sum=0,minLen=100010;
     while (start<=end)
     {
         if(sum>=s){
@@ -23,6 +23,6 @@ int main(){
         else if (end==n) break;
         else sum += arr[end++];
     }
-    cout<<(minLen==100001?0:minLen)<<"\n";
+    cout<<(minLen==100010?0:minLen)<<"\n";
 
 }
