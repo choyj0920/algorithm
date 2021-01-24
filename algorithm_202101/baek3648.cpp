39d9ba35a6bf4f9c4ba748555b0fa2829424dc3d
choyj0920 <44551544+choyj0920@users.noreply.github.com>
2021-01-24 15:56:02 +0900

    // baek3648 아이돌,

--- algorithm_202101/baek3648.cpp
+++ algorithm_202101/baek3648.cpp
@@ -1,96 +1,74 @@
 // baek3648 아이돌
 #include <bits/stdc++.h>
-#include <iostream>
-#include <cstring>
-#include <vector>
-#include <stack>
-#include <utility>
-#include <algorithm>
+#define rep(i,n) for(int i=0;i<n;++i)
+#define REP(i,n) for(int i=1;i<=n;++i)
 #define FAST cin.tie(NULL);cout.tie(NULL); ios::sync_with_stdio(false)
 using namespace std;
-typedef pair<int,int> P;
-const int MAX = 1001;  // n변수 최대 값
-
-int N,M, cnt,scc,dfsn[MAX*2],sccidx[MAX*2];
-vector<int> edge[MAX*2];
-bool finished[MAX*2];
+ 
+#define T(x) (x<<1)
+#define F(x) (x<<1|1)
+ 
+int n, m, a, b, VC, SC;
+ 
+vector<vector<int>> adj; // 간선 정보
+vector<int> discovered, sccid; // 
 stack<int> st;
-
-// 자신의 not literal의 정점 번호 리턴 +,- 전환
-inline int oppo(int n) {return n%2? n-1:n+1;}
-
-int findscc(int cur)
-{
-   dfsn[cur] = ++cnt;
-   int ret= dfsn[cur];
-   st.push(cur);
-
-   for(auto next : edge[cur])
-   {
-       if(dfsn[next] == 0) // 아직 방문 x
-       {
-           ret= min(ret,findscc(next));
-       }
-       else if(!finished[next]) // 
-       {
-           ret = min(ret,dfsn[next]);
-       }
-   }    
-
-   if(ret >= dfsn[cur] && !finished[cur])
-   {
-       while(1)
-       {
-           int stop = st.top();
-           st.pop();
-           sccidx[stop] = scc;
-           finished[stop] = true;
-           if(stop == cur) break;
-       }
-       scc++;
-   }
-
-   return ret;
-}
-int main(){
-    //input
-    FAST;
-    while(cin>>N>>M){
-        cnt=0;
-        memset(dfsn,0,N<<1);
-        memset(finished,false,N<<1);
-        memset(sccidx,0,N<<1);
-        edge->clear();
-        edge->resize(N<<1);
-        st=stack<int>();
-        scc=0;
-        for (int i =0; i<M;i++){
-            int a,b;
-            scanf("%d %d",&a,&b);
-            a = (a<0? -(a+1)*2 : a*2-1);
-            b = (b<0? -(b+1)*2 : b*2-1);
-            edge[oppo(a)].push_back(b);
-            edge[oppo(b)].push_back(a);        
-        }
-
-        // scc
-        for (int i=0; i<N*2;i++){
-            if (dfsn[i]==0) findscc(i);
-        }
-        bool flag=true;
-        for (int i =0;i<N;i++){
-            if (sccidx[i*2]==sccidx[i*2+1]){    //불 가능
-                flag=false;
-                break;
-            }
-        }
-        if (sccidx[1]<sccidx[oppo(1)]){
-            flag=false;
-        }
-
-        cout<<(flag?"yes":"no")<<'\n';
+int scc(int here) {     // scc함수 
+    int ret = discovered[here] = VC++; //정점고유id
+    st.emplace(here);
+ 
+    for (auto there : adj[here]) {
+        if (discovered[there] == -1) ret = min(ret, scc(there)); //아직 발견안한 노드 scc
+        else if (sccid[there] == -1) ret = min(ret, discovered[there]); // 발견한노드 부모 혹은 형제
     }
-
-
-
+ 
+    if (ret == discovered[here]) { //부모노드면
+        while (1) {// st 나올때까지
+            int t = st.top();   
+            st.pop();
+            sccid[t] = SC; // 그룹 id
+            if (t == here) break; //부모노드(자신)나올 떄 가지
+        }
+        SC++; //그룹id
+    }
+    return ret;
+}
+ 
+void OR(int A, int B) {
+    adj[A ^ 1].emplace_back(B);
+    adj[B ^ 1].emplace_back(A);
+}
+ 
+int main() {
+    FAST;
+    while (cin >> n >> m) {
+        VC = SC = 0;
+        adj.clear();
+        adj.resize(n << 1);
+        discovered.clear();
+        sccid.clear();
+        discovered = sccid = vector<int>(n << 1, -1);
+ 
+        while (m--) { // 심사위원 심사 입력
+            cin >> a >> b;
+            a = a > 0 ? T(a - 1) : F(~a);
+            b = b > 0 ? T(b - 1) : F(~b);
+            OR(a, b);
+        }
+ 
+        rep(i, n << 1) if (discovered[i] == -1) scc(i); // scc 수행
+ 
+        bool flag = true;
+        for (int i = 0;i < n;++i) if (sccid[i] == sccid[i ^ 1]) {   // 한 그룹안에 참 거짓 존재 바로 사형
+            flag = false;
+            break;
+        }
+ 
+        if (sccid[0] > sccid[1]) flag = false;      // 위상정렬 상에서 1번 합격이 1번 불합보다 더 빨라야
+ 
+        cout << (flag ? "yes" : "no") << '\n';
+ 
+    }
+ 
+    return 0;
 }
