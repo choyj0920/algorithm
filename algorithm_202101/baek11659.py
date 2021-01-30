dd4e5edc8dddfdbad29c2be561ab7171cb19ddfa
choyj0920 <44551544+choyj0920@users.noreply.github.com>
2021-01-30 20:59:28 +0900

    # baek11659 구간 합 구하기 4

---
+++ algorithm_202101/baek11659.py
@@ -0,0 +1,13 @@
+# baek11659 구간 합 구하기 4
+import sys
+input=sys.stdin.readline
+
+n,m=map(int,input().split())
+arr=[0]+list(map(int,input().split()))
+for i in range(1,n+1):
+    arr[i]+=arr[i-1]
+for i in range(m):
+    a,b=map(int,input().split())
+    print(arr[b]-arr[a-1])
+
+
