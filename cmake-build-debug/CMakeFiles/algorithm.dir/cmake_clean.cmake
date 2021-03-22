f0206073fe9cb6ab3ecffd2dcdb9c6bd07e295a2
choyj0920 <44551544+choyj0920@users.noreply.github.com>
2021-03-22 13:22:38 +0900

    fix

---
+++ cmake-build-debug/CMakeFiles/algorithm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algorithm.dir/algorithm_202103/baek2336.cpp.obj"
+  "algorithm.exe"
+  "algorithm.exe.manifest"
+  "algorithm.lib"
+  "algorithm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/algorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
