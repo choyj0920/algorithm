91c7a0159b56d9e698f379de8eb8c01f98116737
choyj0920 <44551544+choyj0920@users.noreply.github.com>
2021-04-12 11:16:48 +0900

    # baek11443 짝수번쨰 피보나치 수의 합

---
+++ algorithm_202104/baek11443.py
@@ -0,0 +1,25 @@
+# baek11443 짝수번쨰 피보나치 수의 합
+import sys 
+input=sys.stdin.readline
+MOD=1_000_000_007
+fibobase=[[1,1],[1,0]]
+N=int(input())//2
+
+def matrixMul(a,b):
+    temp= [
+        [(a[0][0]*b[0][0]+a[0][1]*b[1][0]) % MOD,(a[0][0]*b[0][1]+a[0][1]*b[1][1]) % MOD],
+        [(a[1][0]*b[0][0]+a[1][1]*b[1][0]) % MOD,(a[1][0]*b[0][1]+a[1][1]*b[1][1]) % MOD]
+        ]
+    return temp
+
+def pow(n):
+    result=[[1,0],[0,1]]
+    cur =fibobase
+    while n:
+        if n & 1:
+            result = matrixMul(result,cur)
+        cur =matrixMul(cur,cur)
+        n //= 2
+    return result
+
+print((pow((2*N))[0][0]-1)%MOD)
