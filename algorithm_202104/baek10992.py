eebbd5dd3eb908eb2de6901f5ce025314c1200fe
choyj0920 <44551544+choyj0920@users.noreply.github.com>
2021-04-24 20:57:22 +0900

    # baek10992 별 찍기 -17

---
+++ algorithm_202104/baek10992.py
@@ -0,0 +1,9 @@
+# baek10992 별 찍기 -17
+N = int(input())
+for i in range(1, N+1):
+    if i == N:
+        print('*'*(2*i-1))
+    elif i == 1:
+        print(' '* (N-i),'*',sep = '')
+    else:
+        print(' '* (N-i),'*',' '*(2*i-3),'*', sep = '')
