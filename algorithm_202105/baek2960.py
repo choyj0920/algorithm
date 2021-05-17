6bae22b1360bc8fc5bc5b81c953d7b1322e9ce0c
choyj0920 <44551544+choyj0920@users.noreply.github.com>
2021-05-17 09:07:57 +0900

    # baek2960 에라토스테네스의 체

---
+++ algorithm_202105/baek2960.py
@@ -0,0 +1,12 @@
+# baek2960 에라토스테네스의 체
+N, K = map(int, input().split()) 
+cnt = 0 
+nums = [True] * (N + 1) 
+for i in range(2, len(nums) + 1): 
+    for j in range(i, N+1, i): 
+        if nums[j] == True: 
+            nums[j] = False 
+            cnt = cnt + 1
+            if cnt == K: 
+                print(j) 
+                break
