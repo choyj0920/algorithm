72300bc629ca294b9b3d364de64d262ee4f7377e
choyj0920 <44551544+choyj0920@users.noreply.github.com>
2021-05-27 10:08:31 +0900

    # baek9466 텀 프로젝트

---
+++ algorithm_202105/baek9466.py
@@ -0,0 +1,30 @@
+# baek9466 텀 프로젝트
+import sys
+sys.setrecursionlimit(99999)
+input=sys.stdin.readline
+def dfs(node):
+    global ans
+    visited[node]=True
+    team.append(node)
+    num=arr[node]
+
+    if visited[num]:
+        if num in team:
+            ans+= team[team.index(num):]
+        return 
+    else:
+        dfs(num)
+        
+for t in range(int(input())):
+    n=int(input())
+    arr=[0]+list(map(int,input().split()))
+    visited=[True]+[False]*(n)
+    ans=[]
+    for i in range(1,n+1):
+        if visited[i] is False:
+            team=[]
+            dfs(i)
+    print(n-len(ans))
+
+
+    
