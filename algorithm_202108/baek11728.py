ce3c78323cd22a4c1bd1e1812cc46e6ca9319964
choyj0920 <44551544+choyj0920@users.noreply.github.com>
2021-07-19 10:35:42 +0900

    # baek11728 배열 합치기

---
+++ algorithm_202108/baek11728.py
@@ -0,0 +1,9 @@
+# baek11728 배열 합치기
+import sys
+
+a, b = map(int, sys.stdin.readline().split())
+a_list = list(map(int, sys.stdin.readline().split()))
+b_list = list(map(int, sys.stdin.readline().split()))
+answer_list = a_list + b_list
+answer = ' '.join(map(str, sorted(answer_list)))
+print(answer)
